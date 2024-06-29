32af47c566a1a34b7977d947088475321de0fd25
Daniel Amyx <danielamyx859@gmail.com>
2024-06-29 16:39:50 -0400

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'signup', component: SignupComponent},
+    { path: 'login', component: LoginComponent},
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
