336bf94634fac3763758b0873593de0116f7eea4
Daniel Amyx <danielamyx859@gmail.com>
2024-07-02 12:54:31 -0400

    styled loging/signup rough draft

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,12 +12,20 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private authService: AuthService) {}
+  title = 'login'
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
 
   onLogin(form: NgForm) {
     if (form.invalid) {
       return;
     }
     this.authService.login(form.value.email, form.value.password);
+  }
+
+  signupClick() {
+    this.router.navigate(['/signup']) // this is a backup redirect in case the href ever messes up.
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -12,12 +12,18 @@ import { RouterLink } from '@angular/router';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,) {}
 
   onSignup(form: NgForm) {
     if (form.invalid) {
       return;
     }
     this.authService.signup(form.value.email, form.value.password);
+  }
+
+  loginClick() {
+    this.router.navigate(['/login']) // this is a backup redirect in case the href ever messes up.
   }
 }
