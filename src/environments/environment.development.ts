5c907a72fa860e953c670041e32f040676f95d61
Daniel Amyx <danielamyx859@gmail.com>
2024-10-27 00:15:16 -0400

    Wife finally helped

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDUy3cbc_Q0pAXVMox0LUqpu3J8-pD3zLU",
+    authDomain: "yycs-e9632.firebaseapp.com",
+    projectId: "yycs-e9632",
+    storageBucket: "yycs-e9632.appspot.com",
+    messagingSenderId: "953490330687",
+    appId: "1:953490330687:web:44a8cdd0330f02a0f3d48a",
+    measurementId: "G-NL2CBHDKWG"
+  }
+};
