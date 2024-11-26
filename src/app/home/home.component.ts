c1d08a35c467b0911f296ad5390e4a8c1064ddb6
Daniel Amyx <danielamyx859@gmail.com>
2024-11-26 11:59:41 -0500

    updated home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   title = 'home';
   constructor(
     private router: Router,
@@ -20,5 +20,50 @@ export class HomeComponent {
 
   onSubmitProducts() {
     this.router.navigate(['/product'])
+  }
+
+  slideIndex: number = 1;
+
+  ngOnInit() {
+    this.showSlides(this.slideIndex);
+  }
+
+  // Next/previous controls
+  plusSlides(n: number) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n: number) {
+    const slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    const dots = document.getElementsByClassName("demo") as HTMLCollectionOf<HTMLElement>;
+    const captionText = document.getElementById("caption");
+
+    if (n > slides.length) { this.slideIndex = 1; }
+    if (n < 1 ) { this.slideIndex = slides.length; }
+
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    if (slides[this.slideIndex -1]) {
+      slides[this.slideIndex - 1].style.display = "block";
+    }
+
+    if (dots[this.slideIndex - 1]) {
+      dots[this.slideIndex - 1].className += " active";
+    }
+    
+    if (captionText && dots[this.slideIndex -1]) {
+      captionText.innerHTML = (dots[this.slideIndex -1] as HTMLElement).getAttribute('alt') || '';
+    }
   }
 }
